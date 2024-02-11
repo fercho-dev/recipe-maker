5c4dcd557af941ccdbec79714bf2cc680ead8472
Fernando Campos <ferchoeth@gmail.com>
2024-02-10 22:07:24 -0600

    testing msg webhook

--- app/api/whats/webhooks/route.js
+++ app/api/whats/webhooks/route.js
@@ -21,3 +21,9 @@ export async function GET(request) {
     return NextResponse.json({ message: `Invalid Token` }, { status: 400 })
   }
 }
+
+export async function POST(req) {
+  const data = await req.json();
+  console.log(data);
+  return new Response("Hello, Next.js!");
+}
